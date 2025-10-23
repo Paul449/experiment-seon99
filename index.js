6964bd1a8272b97dc5ed71e8acca3134dac32042
paul449 <paul_bilbatua@hotmail.com>
2025-10-23 16:07:03 -0500

    u

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+//import the entire three.js library
 import * as THREE from 'three';
+//fuction two accept two inputs front head, and side head
+function getUserInfo(frontHead, sideHead) {
+    //create a scene
+}
