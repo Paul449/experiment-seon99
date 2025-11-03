a3b35937171a52ca0ec78b332fcd75755f00568a
paul449 <paul_bilbatua@hotmail.com>
2025-11-03 08:28:00 -0600

    swift finnaly worked

--- Module3DTest/metro.config.js
+++ Module3DTest/metro.config.js
@@ -1,22 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// For local development
-if (process.env.NODE_ENV !== 'production') {
-  // Add the parent directory to watch folders so Metro can find the local module
-  config.watchFolders = [
-    path.resolve(__dirname, '../Module3D'),
-    path.resolve(__dirname, '../Module3D/node_modules'),
-  ];
-
-  // Add node modules paths
-  config.resolver.nodeModulesPaths = [
-    path.resolve(__dirname, 'node_modules'),
-    path.resolve(__dirname, '../node_modules'),
-  ];
-}
 
 // Ensure Metro can resolve the local module
 config.resolver.platforms = ['ios', 'android', 'native', 'web'];
