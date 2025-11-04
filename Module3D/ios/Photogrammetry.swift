674183039a870628a3712f8005b939ae62814cda
paul449 <paul_bilbatua@hotmail.com>
2025-11-04 11:44:56 -0600

    configuring session

--- Module3D/ios/Photogrammetry.swift
+++ Module3D/ios/Photogrammetry.swift
@@ -6,12 +6,25 @@ import Photogrammetry;
 import RealityKit;
 //Creating session for developing 3D models from photos
 public class Photogrammetry: Module{
-    //Creating a session from a set of images
+    //Creating a session from a set of images or default values
     convenience init(){
         //accepts images from a directory
         input: URL,
         //configuration for the session
         configuration:PhotogrammetrySession.Configuration = Configuration(),
         throws: Error
+    }
+    //check if actually hardware supports photogrammetry(iphone 12 and above or Ipad)
+    static func isSupported() -> Bool{
+        return PhotogrammetrySession.isSupported
+    }
+    //configure session
+    var configuration: PhotogrammetrySession.Configuration{
+        get{
+            return self.configuration
+        }
+        set(newConfiguration){
+            self.configuration = newConfiguration
+        }
     }
 }
