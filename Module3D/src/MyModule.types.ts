62105bb73012deb3691e9665911fd82eff44a49b
paul449 <paul_bilbatua@hotmail.com>
2025-11-04 15:25:37 -0600

    deleting extra native module

--- Module3D/src/MyModule.types.ts
+++
@@ -1,9 +0,0 @@
-export type MyModuleEvents = {
-  onChange: (params: { value: string }) => void;
-};
-
-export type MyModuleViewProps = {
-  url: string;
-  onLoad: (event: { nativeEvent: { url: string } }) => void;
-  style?: any;
-};
