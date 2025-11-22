b343beab81374f59236ef29dac86b5cd8f7cf0a4
bilgersf <rodbilger@gmail.com>
2025-11-22 14:23:13 -0600

    wip

---
+++ Hailuo/APIConfig.js
@@ -0,0 +1,9 @@
+const API_Config = {
+    API_SECRET_KEY: `46f63100dc483500b1345da2df6e51756a392350d7fb13041112282df14b38dd`,
+    API_KEY_ID: `74521134-a405-4d71-8fdc-099a9da33d11`,
+    MODEL_URI: 'minimax/hailuo-02/pro',
+    BASE_URL: 'https://platform.higgsfield.ai',
+    PROMPT: 'orbit',
+}
+
+export default API_Config;
